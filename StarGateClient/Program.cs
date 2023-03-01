fb22c44fe61278dee53ed44dd2a339da1c7bff0a
Lukáš Vámoš <lukas.vamos@gmail.com>
2023-03-01 17:15:21 +0100

    *feat: add the client console app that displays text messages from the server

---
+++ StarGateClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+// Displays a prompt and terminates the program.
+void Terminate(string message = "")
+{
+	Console.WriteLine(message);
+	Console.WriteLine("Press enter to terminate.");
+	Console.ReadLine();
+	Environment.Exit(0);
+}
+
+static void ReceiveMessage(string title, string message) => Console.WriteLine($"{title}: {message}");
+
+// Establish SignalR connection
+HubConnection connection = new HubConnectionBuilder()
+	.WithUrl("https://localhost:7230/RequestQueueHub")
+	.Build();
+
+connection.Closed += async (error) => Terminate(error.Message);
+
+connection.On<string, string>("ReceiveMessage", ReceiveMessage);
+
+
+Console.WriteLine("Establishing connection");
+try
+{
+	await connection.StartAsync();
+
+}
+catch
+{
+	Terminate("Unable to establish connection to the server.");
+}
+
+Console.WriteLine("Listening to the server. Press any key to stop.");
+while (true)
+{
+	await Task.Delay(10);
+	if (Console.ReadKey().Key == ConsoleKey.Enter)
+	{
+		connection.StopAsync();
+		Terminate("Connection ended.");
+		break;
+	}
+}
